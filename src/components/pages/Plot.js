0c9700f252009cb58f07374240a3097e7642c05d
WillUniverse <wendelsantosufpa@gmail.com>
2020-10-15 18:08:16 -0300

    feat(plot): criar seletor de data  no componente Plot
    o seletor de data será usado para escolha do dia a ser visualizado no histórico de dados

--- src/components/pages/Plot.js
+++ src/components/pages/Plot.js
@@ -2,12 +2,31 @@ import React, { useState, useEffect } from 'react'
 import NavBar from '../panels/NavBar'
 import Menu from '../panels/Menu'
 import Chart from '../panels/Chart'
+import Export from '../panels/Export'
 
 import { getData } from '../../services/storage'
 import fetch from '../../services/fetch'
 
 import '../../styles/plot.css'
 import '../../styles/util.css'
+
+const data = [
+    ['firstname', 'lastname', 'email'],
+    ['Ahmed', 'Tomi', 'ah@smthing.co.com'],
+    ['Raed', 'Labes', 'rl@smthing.co.com'],
+    ['Yezzi', 'Min l3b', 'ymin@cocococo.com']
+]
+
+const tempCollection = [{
+    title: 'Dispositivo 1',
+    timestamps: new Array(50).fill('2:00'),
+    datasets: [
+        {
+            label: 'teste1',
+            data: new Array(50).fill('5')
+        }
+    ]
+}]
 
 const Plot = ({ history }) => {
     const params = history
@@ -20,7 +39,21 @@ const Plot = ({ history }) => {
     const [deviceIndex, setDeviceIndex] = useState(params[1])
     const [loading, setLoading] = useState(true)
     const [success, setSuccess] = useState(false)
+    // eslint-disable-next-line
+    const [currentDate, setCurrentDate] = useState(
+        `${
+            new Date().getFullYear()
+        }-${
+            new Date().getMonth() < 10 ?
+                '0' + new Date().getMonth()
+                :
+                new Date().getMonth()
+        }-${
+            new Date().getDate()
+        }`
+    )
 
+    console.log(currentDate)
     useEffect(() => {
         (async () => {
             if(await fetch(
@@ -49,20 +82,46 @@ const Plot = ({ history }) => {
                 setItemIndex={setConsumerUnitIndex}
                 setSubItemIndex={setDeviceIndex}
             />
-            {!loading ?
-                success ?
-                    <div className='chart-container'>
-                        <Chart collection={getData('collection')} />
-                    </div>
-                    :
-                    <div className='empty'>
-                        <p>Não há dados deste dispositivo</p>
-                    </div>
-                :
-                <div className='loading-container'>
-                    <progress className='circular-progress'/>
+            <div className='content-container'>
+                <div className='date-picker'>
+                    <input
+                        type='date'
+                        defaultValue={currentDate}
+                    />
+
+                    <button className='classic-button'>
+                        OK
+                    </button>
                 </div>
-            }
+                {loading ?
+                    !success ?
+                        <div className='chart-container'>
+                            <Chart collection={tempCollection} />
+                        </div>
+                        :
+                        <div className='empty'>
+                            <p>Não há dados deste dispositivo</p>
+                        </div>
+                    :
+                    <div className='loading-container'>
+                        <progress className='circular-progress'/>
+                    </div>
+                }
+                <div className='buttons'>
+                    <button
+                        className='classic-button'
+                        onClick={() => {
+                            history.push('/dashboard')
+                        }}
+                    >
+                        Voltar
+                    </button>
+                    {!loading ?
+                        <Export data={data}/>
+                        : null
+                    }
+                </div>
+            </div>
         </div>
 
     </div>
--- src/components/panels/Export.js
+++ src/components/panels/Export.js
@@ -3,22 +3,13 @@ import { CSVLink } from 'react-csv'
 
 import '../../styles/export.css'
 
-// const data = [
-//     ['firstname', 'lastname', 'email'],
-//     ['Ahmed', 'Tomi', 'ah@smthing.co.com'],
-//     ['Raed', 'Labes', 'rl@smthing.co.com'],
-//     ['Yezzi', 'Min l3b', 'ymin@cocococo.com']
-// ]
-
 const Export = ({ data }) =>
-    <div className='export'>
-        <CSVLink
-            className='csv-link'
-            data={data}
-            filename={'uirapuru-chart.csv'}
-        >
-            Download
-        </CSVLink>;
-    </div>
+    <CSVLink
+        className='export'
+        data={data}
+        filename={'uirapuru-chart.csv'}
+    >
+        Download
+    </CSVLink>
 
 export default Export
