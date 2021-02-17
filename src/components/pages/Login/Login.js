85bdd2226ba6fe2f1f348b64a1e2420fd94bb795
Fernando Paiva <fernandopaiva@techamazon.tech>
2021-02-17 11:54:21 -0300

    fix(login.js): a label do campo de senha tinha sido removida

--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -75,6 +75,9 @@ const Login = ({ history }) => {
                 }}
             />
 
+            <styles.label htmlFor='password'>
+                Senha
+            </styles.label>
             <styles.password>
                 {showPassword ?
                     <HidePasswordIcon
